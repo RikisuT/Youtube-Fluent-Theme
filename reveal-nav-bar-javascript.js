8be2a021ddb6cd2c9a5f65d59fe933d492c1001e
Eason Wong <eason.wong.yisum@gmail.com>
2023-02-07 08:27:50 +0100

    Even better
    See comments in codes

--- reveal-nav-bar-javascript.js
+++ reveal-nav-bar-javascript.js
@@ -2,15 +2,21 @@ let guide = document.querySelector('#guide');
 let container = document.querySelector('#contentContainer');
 let button = document.querySelector('#guide-button');
 
+//Hides nav bar when loaded
+
 guide.removeAttribute("guide-persistent-and-visible");
 guide.removeAttribute("opened");
 guide.setAttribute("mini-guide-visible", "");
 container.removeAttribute("opened");
 
-button.addEventListener("click", () => {
-  guide.setAttribute("reveal-nav-bar", "");
-});
+//Allow transitions to play
 
-document.addEventListener('afterscriptexecute', () => {
-  window.dispatchEvent(new Event('resize'));
-});
+button.addEventListener("click", () => guide.setAttribute("reveal-nav-bar", ""));
+
+//Re-arrange thumbnails
+
+document.addEventListener('afterscriptexecute', () => window.dispatchEvent(new Event('resize')));
+
+//When URL changes, hide nav bar
+
+new MutationObserver(mutations => button.click()).observe(document.querySelector('title'), { childList: true });
