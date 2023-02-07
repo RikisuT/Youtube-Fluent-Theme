b2b37fac6f9ca18ea6b783e1cdecaa21f46388ea
Eason Wong <eason.wong.yisum@gmail.com>
2023-02-07 07:40:56 +0100

    Optimisations
    Smoother animations, blur effects etc.

--- reveal-nav-bar-javascript.js
+++ reveal-nav-bar-javascript.js
@@ -1,9 +1,16 @@
-document.getElementById('guide').style.display = 'none';
+let guide = document.querySelector('#guide');
+let container = document.querySelector('#contentContainer');
+let button = document.querySelector('#guide-button');
 
-document.getElementById('guide-button').addEventListener('click', () => {
-	document.getElementById('guide').style.display = 'block';
+guide.removeAttribute("guide-persistent-and-visible");
+guide.removeAttribute("opened");
+guide.setAttribute("mini-guide-visible", "");
+container.removeAttribute("opened");
+
+button.addEventListener("click", () => {
+  guide.setAttribute("reveal-nav-bar", "");
 });
 
 document.addEventListener('afterscriptexecute', () => {
-	window.dispatchEvent(new Event('resize'));
+  window.dispatchEvent(new Event('resize'));
 });
