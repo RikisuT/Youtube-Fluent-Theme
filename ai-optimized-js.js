4e5820e4ac6dd33c85787328e4b3c4e332912f2f
Rikisu <47000780+RikisuT@users.noreply.github.com>
2023-04-08 21:05:10 +0530

    Update and rename optimized-js.js to ai-optimized-js.js

--- ai-optimized-js.js
+++ ai-optimized-js.js
@@ -18,17 +18,3 @@ new MutationObserver(mutations => {
     button.click();
   }
 }).observe(document.querySelector('title'), { childList: true });
-
-// Add 'homepage' class to header element if on homepage
-const header = document.querySelector('div#header.ytd-app');
-if (header) {
-  function updateHeaderDisplay() {
-    if (window.location.pathname === '/') {
-      header.style.display = 'flex';
-    } else {
-      header.style.display = 'none';
-    }
-    requestAnimationFrame(updateHeaderDisplay);
-  }
-  updateHeaderDisplay();
-}
